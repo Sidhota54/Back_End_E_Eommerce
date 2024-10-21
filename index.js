117a6b0d412c928ed91d897f6c6f4a1483045169
Siddharth Hota <siddharth.hota@incture.com>
2024-10-21 10:47:48 +0530

    Update code

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Enable CORS for all routes
-// app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 
 // Handle pre-flight requests for all routes
 // app.options('*', cors());
@@ -40,20 +40,20 @@ const PORT = process.env.PORT || 5000;
 //     credentials: true // Optional: Allow credentials if needed
 //   })
 // );
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST", "DELETE", "PUT"],
+//     allowedHeaders: [
+//       "Content-Type",
+//       "Authorization",
+//       "Cache-Control",
+//       "Expires",
+//       "Pragma",
+//     ],
+//     credentials: true,
+//   })
+// );
 
 app.use(cookieParser());
 app.use(express.json());
